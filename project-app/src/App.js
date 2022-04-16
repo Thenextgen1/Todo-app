033ec6134c0fd7a681abb309c9b18d23d0b9ac10
Elijah Ohiwerei <elijahohimairei@gmail.com>
2022-04-16 23:32:05 +0100

    building headers

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -6,14 +6,6 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
-
-
-      <h1 className="text-3xl text-center font-bold underline">
-        Hello world!
-      </h1>
-      <button class="bg-blue-500 center hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
-        Button
-      </button>
       <Header />
       <Todo />
 
--- project-app/src/components/Header/Header.js
+++ project-app/src/components/Header/Header.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sun from '../../assets/images/icon-sun.svg'
 import moon from '../../assets/images/icon-moon.svg'
 import darkbackgrounddesktop from '../../assets/images/bg-desktop-dark.jpg'
 import darkbackgroundmobile from '../../assets/images/bg-mobile-dark.jpg'
 import lightbackgrounddesktop from '../../assets/images/bg-desktop-light.jpg'
 import lightbackgroundmobile from '../../assets/images/bg-mobile-light.jpg'
+import styled from 'styled-components'
+import './Header.css'
 
+const StyledHeader = styled.header`
+padding: 4.5em 0 1em 0;
+height: 250px;
+`
 
 
 
 const Header = () => {
+    const [background, setBackground] = useState({
+        lightmode: true,
+    })
+    const style = {
+        backgroundImage: `url(${background.desktop})`
+    }
+
+    const setTheme = () => {
+
+        setBackground(prevBackground => {
+            return {
+                ...prevBackground,
+                lightmode: !background.lightmode,
+            }
+        })
+    }
+    background.lightmode ? style.backgroundImage = `url(${lightbackgrounddesktop})` : style.backgroundImage = `url(${darkbackgrounddesktop})`
+
+
     return (
-        <div></div>
+        <StyledHeader className="flex justify-between bg-no-repeat" style={style}>
+            <h1 className='relative text-3xl font-bold text-white'>TODO</h1>
+            <img className='relative' src={background.lightmode ? moon : sun} alt="selected mode" onClick={setTheme} />
+        </StyledHeader>
     )
 }
 
